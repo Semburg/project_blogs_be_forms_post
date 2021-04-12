543de8a3d4b19ab4504ee52df7475fec6b9ab51e
Semburg <semburg.sb@gmail.com>
2021-04-12 13:26:15 +0200

    before post new

---
+++ index.js
@@ -0,0 +1,62 @@
+const express = require('express');
+
+const fs = require('fs');
+let data = []
+if (fs.existsSync('./blogs.json')) {
+    data = require('./blogs.json')
+}
+
+const app = express();
+const port = 5000
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs')
+
+//json paring
+app.use(express.urlencoded({ extended: false }))
+app.use(express.json())
+
+app.listen(port, ()=>{
+    console.log(`Server running on port: ${port} `);
+});
+
+
+app.get('/', (req, res)=>{
+    res.render('pages/index', {blogs:data});
+})
+
+app.get("/post/:id", (req, res)=>{
+    
+    console.log(req.params)
+    // console.log(JSON.parse(req.params))
+
+
+    fs.readFile("./blogs.json", 'utf-8', (err, data)=>{
+        if(err){
+            console.log(`Error: ${err}`);
+        } else {
+            const database = JSON.parse(data);
+            const postId = req.params.id
+
+            console.log(database[postId]);
+
+            res.render("pages/post", {post: database[postId]})
+
+
+            // res.send(database[postId].title)
+
+
+            // database.forEach(post => {
+            //     console.log(post.id);
+            //     console.log(post.title);
+            // });
+        }
+    })
+
+    
+
+
+
+})
+
