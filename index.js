efc6c64b600ae3f4352aeb148670b49639bf6f47
Semburg <semburg.sb@gmail.com>
2021-04-13 17:47:59 +0200

    done 2

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ if (fs.existsSync('./blogs.json')) {
 }
 
 const app = express();
-const port = 5000
+const PORT = process.env.PORT || 5000
 
 app.use(express.static('public'))
 
@@ -19,8 +19,8 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
 
-app.listen(port, () => {
-    console.log(`Server running on port: ${port} `);
+app.listen(PORT, () => {
+    console.log(`Server running on port: ${PORT} `);
 });
 
 
